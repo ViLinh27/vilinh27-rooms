b2977e8122ef7c456bd2a877f959af883c93e7cf
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-05-30 17:40:36 -0500

    fix styling for nav bar

--- linh-rooms/src/pages/Homepage.js
+++ linh-rooms/src/pages/Homepage.js
@@ -3,16 +3,18 @@ import React from 'react';
 function Homepage(){
     return(
         <>
-            <div class="Home-nav-left">
-                <div>nav bar left item</div>
-                <div>nav bar left item</div>
-                <div>nav bar left item</div>
-            </div>
-            <div class="Home-nav-right">
-                <div>nav bar right item</div>
-                <div>nav bar right item</div>
-                <div>nav bar right item</div>
-                <div>nav bar right item</div> 
+            <div className="Home-nav">
+                <div className="Home-nav-left">
+                    <div>nav bar left item</div>
+                    <div>nav bar left item</div>
+                    <div>nav bar left item</div>
+                </div>
+                <div className="Home-nav-right">
+                    <div>nav bar right item</div>
+                    <div>nav bar right item</div>
+                    <div>nav bar right item</div>
+                    <div>nav bar right item</div> 
+                </div>
             </div>
             <header className="Home-main">
                 <div>test door</div>
