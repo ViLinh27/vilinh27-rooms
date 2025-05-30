96353b44cb11cc3b35eedce1ff19277226b1cfd2
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-05-30 17:28:25 -0500

    move divs for homepage to separate component and rename stylings to compensate

--- linh-rooms/src/App.js
+++ linh-rooms/src/App.js
@@ -1,28 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <div class="App-nav-left">
-        <div>nav bar left item</div>
-        <div>nav bar left item</div>
-        <div>nav bar left item</div>
-      </div>
-      <div class="App-nav-right">
-        <div>nav bar right item</div>
-        <div>nav bar right item</div>
-        <div>nav bar right item</div>
-        <div>nav bar right item</div>
-      </div>
-      <header className="App-main">
-        <div>test door</div>
-        <div>test door</div>
-        <div>test door</div>
-        <div>test door</div>
-        <div>test door</div>
-        <div>test door</div>
-      </header>
+      <Homepage/>
     </div>
   );
 }
---
+++ linh-rooms/src/components/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Homepage(){
+    return(
+        <>
+            <div class="Home-nav-left">
+                <div>nav bar left item</div>
+                <div>nav bar left item</div>
+                <div>nav bar left item</div>
+            </div>
+            <div class="Home-nav-right">
+                <div>nav bar right item</div>
+                <div>nav bar right item</div>
+                <div>nav bar right item</div>
+                <div>nav bar right item</div> 
+            </div>
+            <header className="Home-main">
+                <div>test door</div>
+                <div>test door</div>
+                <div>test door</div>
+                <div>test door</div>
+                <div>test door</div>
+                <div>test door</div>
+            </header>
+        </>
+    );
+}
+
+export default Homepage;
