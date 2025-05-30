056c74882059e7028b345e3299fdbde87f79b485
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-05-30 17:47:29 -0500

    put nav bar content into separate component

---
+++ linh-rooms/src/components/homepage/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <>
+            <div className="Home-nav">
+                <div className="Home-nav-left">
+                    <div>nav bar left item</div>
+                    <div>nav bar left item</div>
+                    <div>nav bar left item</div>
+                </div>
+                <div className="Home-nav-right">
+                    <div>nav bar right item</div>
+                    <div>nav bar right item</div>
+                    <div>nav bar right item</div>
+                    <div>nav bar right item</div> 
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
--- linh-rooms/src/pages/Homepage.js
+++ linh-rooms/src/pages/Homepage.js
@@ -1,21 +1,10 @@
 import React from 'react';
+import Navbar from '../components/homepage/Navbar';
 
 function Homepage(){
     return(
         <>
-            <div className="Home-nav">
-                <div className="Home-nav-left">
-                    <div>nav bar left item</div>
-                    <div>nav bar left item</div>
-                    <div>nav bar left item</div>
-                </div>
-                <div className="Home-nav-right">
-                    <div>nav bar right item</div>
-                    <div>nav bar right item</div>
-                    <div>nav bar right item</div>
-                    <div>nav bar right item</div> 
-                </div>
-            </div>
+            <Navbar/>
             <header className="Home-main">
                 <div>test door</div>
                 <div>test door</div>
