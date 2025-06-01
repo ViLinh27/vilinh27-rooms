9dd7d9fe9be1c2d8bd6cc81952149cbb3ff6b8de
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-06-01 09:24:52 -0500

    add hover open door effect to doors
    move door stylings to separate door.css file

--- linh-rooms/src/components/Door.js
+++ linh-rooms/src/components/Door.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import '../DoorStyles.css'
 
 function Door(props){
+    //need to track state of hovering on door and outside door
+    const [isHovering, setIsHovering] = useState(false);
+
+    //how we see which image to use based on hover state
+    //notice the tertiary statement
+    //if isHovering is true, use open door asset,
+    //if not use closed door asset
+    const currImgInUse = isHovering ? '/assets/images/opendoorway.webp' : '/assets/images/room-doors.webp';
+
+    //handles when we toggle setIsHovering state
+    const handleMouseEnter = ()=>{
+        setIsHovering(true);
+    }
+    const handleMouseLeave = () =>{
+        setIsHovering(false);
+    }
+    
     return(
         <>
-            <div className="Home-Door">
+            <div 
+                className="Home-Door"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >{/*note the event handlers in the divs */}
                 <a href={props.linkUrl} target="_blank" rel="noopener noreferrer">
                      {/* Note
                             target _blank means link url will open new tab 
                             rel noopener noreferrer is for secuirty when opening links to new tabs
                     */}
-                    <img className="Home-DoorImg" src="/assets/images/room-doors.webp" alt="door"/>
-                    <div>{props.captionText}</div>
+                    <img className="Home-DoorImg" src={currImgInUse} alt={isHovering ? "open door" : "closed door"}/>
+                    <div className="Home-Door-caption">{props.captionText}</div>
                 </a>
             </div>
         </>
