d37fe8049571af56612e557cecdd6377b5ef3030
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-06-03 19:29:06 -0500

    add self portrait
    needs to be styled

--- linh-rooms/src/pages/About.js
+++ linh-rooms/src/pages/About.js
@@ -4,7 +4,13 @@ function About(){
     return(
         <>
             <div className="App">
-            Test content for about page
+                <div className="About">
+                    <div className="About-text">
+                        <h2>Who am I?</h2>
+                        <p>I go by Linh.</p>
+                    </div>
+                    <img className="About-img" src="/assets/images/self-portrait.webp" alt="self portrait"/>
+                </div>
             </div>
         </>
     )
