fc47239db723633c05e36c9e440fb9f102fb1942
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-06-04 14:58:42 -0500

    Add in base plate info for cotnact page

--- linh-rooms/src/pages/Contact.js
+++ linh-rooms/src/pages/Contact.js
@@ -4,7 +4,16 @@ function Contact(){
     return(
         <>
         <div className="App">
-            Test content for contact page
+            <div className="MyContact">
+                <h1>I'm gonna get a letter!</h1>
+                <p>The easiest way to contact me is through email.</p>
+                <p>I will figure out how to make a form to send directly to my email. 
+                    But for now this page will have to do.
+                    I look forward to hearing from you :D.
+                </p>
+
+                <p>Email me at: vilinh27@gmail.com </p>
+            </div>
         </div>
     </>
     );
